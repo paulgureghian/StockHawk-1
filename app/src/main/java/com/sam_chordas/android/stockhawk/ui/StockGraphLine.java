7d9aa212a63cea2afef438dcf9eb5d797d30ab80
Paul <paulgureghian@att.net>
2016-07-04 10:39:32 -0700

    7-4-16  10:30 AM

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/StockGraphLine.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/StockGraphLine.java
@@ -1,11 +1,16 @@
 package com.sam_chordas.android.stockhawk.ui;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.LineChart;
 import com.sam_chordas.android.stockhawk.R;
 
 public class StockGraphLine extends Activity {
+
+    LineChart chart = (LineChart) findViewById(R.id.chart);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

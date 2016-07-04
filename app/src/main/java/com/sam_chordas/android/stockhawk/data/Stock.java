3b303eca940a0b53561423ecd8bb3d33f932f2ee
Paul <paulgureghian@att.net>
2016-07-04 16:26:52 -0700

    7-4-16  4:30 PM

---
+++ app/src/main/java/com/sam_chordas/android/stockhawk/data/Stock.java
@@ -0,0 +1,16 @@
+package com.sam_chordas.android.stockhawk.data;
+
+
+import android.os.Parcelable;
+
+//public class Stock implements Parcelable {
+
+//    private String Symbol;
+//    private String Date;
+//    private String Open;
+//    private String High;
+//    private String Low;
+//    private String Close;
+//    private String Adj_Close;
+//    private String quote;
+//}
